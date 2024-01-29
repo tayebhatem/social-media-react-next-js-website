81efb960014535f3b7bc36b35b79f39134f5136d
tayebhtaem <tayebhtaem98@gmail.com>
2024-01-29 17:26:54 +0100

    add facebook auth commit

--- pages/login.js
+++ pages/login.js
@@ -5,14 +5,19 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 export default function Login() {
   const supabase=useSupabaseClient();
+
   const loginWithGoogle=async()=>{
    await supabase.auth.signInWithOAuth({
       provider: 'google',
     })
-    
-    
-    
+  
   }
+  const signInWithFacebook=async()=> {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'facebook',
+    })
+  }
+  
   return (
    
        <div className=" p-3  bg-white rounded-md shadow-md max-w-md mx-6 sm:mx-auto mt-52">
@@ -23,7 +28,7 @@ export default function Login() {
         <FcGoogle className="text-2xl"/>
         Login with Google
         </button>
-        <button className="flex  items-center justify-center gap-2 bg-gray-100 py-3 px-5 rounded-full grow text-lg hover:bg-gray-200">
+        <button className="flex  items-center justify-center gap-2 bg-gray-100 py-3 px-5 rounded-full grow text-lg hover:bg-gray-200" onClick={signInWithFacebook}>
         <FaFacebook className="text-2xl text-blue-700"/>
         Login with Facebook
         </button>
